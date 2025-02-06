f8860f8d56a5be8a5b1ea16821207aee9c6f7906
mexa-bro <dantdm0106@gmail.com>
2025-02-06 09:42:45 +0500

    container

---
+++ src/components/common/container.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Container = (className, children) => {
+  return (
+    <div className={`max-w-[1504px] px-[27px] mx-auto text-white ${className}`}>
+        {children}
+    </div>
+  )
+}
